67ac28922225cc4736c31bb653597234c7624b9b
Guzman ezequiel alejandro <124918739+Sarabe89@users.noreply.github.com>
2023-05-21 22:01:27 -0300

    Create ejercicionumeros.js

---
+++ ejercicionumeros.js
@@ -0,0 +1,17 @@
+// creo una variable con mi altura en centimetro con un entero
+let altura = 178;
+// creo una variable con mi altura en metros en un numero flotante
+let alturaMetros = 1.78;
+// creo una variable de mi peso en kilogramos en un numero flotante
+let pesoKilogramos = 85.500;
+// redondeo la aaltura hacia arriba
+let redAltura = Math.ceil(alturaMetros)
+console.log(redAltura);
+
+// redondeo todos los numeros hacia abajo
+let redPeso = Math.floor(pesoKilogramos)
+console.log(pesoKilogramos);
+
+// le sumo 1 al maximo valor de js y da el mismo numero
+let MAx_valor_JS = Number.MAX_VALUE + 1;
+console.log(MAx_valor_JS);
