ff85be9f678356a32e5a88a76adbfac9da7d935a
Guzman ezequiel alejandro <124918739+Sarabe89@users.noreply.github.com>
2023-05-30 18:43:28 -0300

    Update ejercicioclases.js

--- ejercicioclases.js
+++ ejercicioclases.js
@@ -1,14 +1,15 @@
-
+// creo una clase estudiante con la variable nombre, y asignaturas a la que le dejo por defecto la lista
 class Estudiante    {
     constructor(nombre,asignaturas = ["Javascrip","Html", "Css"])     {
         this.nombre=nombre
         this.asignaturas= asignaturas
     }
-
+//     creo el metodo obtenDatos
     obtenDatos()    {
         return (`el estudiante se llama ${this.nombre} y tiene las asignaturas ${this.asignaturas}`)
     }
 }
-
+// creo una instancia de estudiante
 const estudiante1 = new Estudiante("juan")
+// uso el metodo obtenDatos y lo muestro por pantalla
 console.log(estudiante1.obtenDatos());
