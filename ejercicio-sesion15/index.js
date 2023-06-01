0ecbd263f254fdbb325d44417616aa1ceba0b14f
Guzman ezequiel alejandro <124918739+Sarabe89@users.noreply.github.com>
2023-06-01 16:47:45 -0300

    Add files via upload

---
+++ ejercicio-sesion15/index.js
@@ -0,0 +1,23 @@
+
+// creo una variable con mi nombre y apellido
+const nombre = "ezequiel"
+const Apellido = "guzman"
+// creo un objeto con las variables anteriores
+const nombreCompleto =  {
+    nombre,
+    Apellido
+}
+// almaceno en la sesion Store
+// sessionStorage.setItem("objeto", JSON.stringify(nombreCompleto))
+
+// // almaceno en la localStore
+// localStorage.setItem("objetoLocal",JSON.stringify(nombreCompleto))
+
+// // cookie
+// const fechaDeExpiracion = new Date();
+// fechaDeExpiracion.setTime(fechaDeExpiracion.getTime()+ (2 * 60 * 1000));
+
+
+// document.cookie = "nombre:EzequielGuzman;expires" + fechaDeExpiracion.toUTCString();
+
+
